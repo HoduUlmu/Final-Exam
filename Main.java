d36ce9129d5f9bcb0fabb9234a4cf0e782ab983e
HoduUlmu <snoopyd@inu.ac.kr>
2020-06-27 11:40:56 +0900

    second

--- Main.java
+++ Main.java
@@ -5,56 +5,65 @@ import static java.lang.Math.pow;
 
 public class Main {
 
+    //b는 정해놓은 값이기에 전역변수로 설정해준다.
+    public static double b = 100;
 
+
+    //메인함수에서 데이터 생성을 해주고 selection,crossover,mutation을 실행한다.
     public static void main(String[] args) {
+
+        //데이터셋 생성
+        double a = 1.5;
+        ArrayList<Double> xu = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            xu.add(i,Math.random()*(35-20+1)+20);
+        }
+
+        Random ran = new Random();
+        ArrayList<Double> yu = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            yu.add(i, a*xu.get(i)+b+ran.nextGaussian()*4);
+        }
+
+        //a,기울기의 후보값들 생성
         double d = (double)(3)/(double)10000;
         ArrayList<Double> au = new ArrayList<>();
         for (int i = 0; i < 10000; i++) {
             au.add(i,(i+1)*d);
         }
 
-        for (int i  = 0; i <1000 ; i++) {
-            int result = hubo(au);
-            int result2 = selection(au, result);
+        //selection을 통해 유지할 n개의 후보값 설정
+        ArrayList<Double> result = new ArrayList<>();
+
+        //init
+        result = selection(xu,yu,au);
+
+        //반복수행
+        for (int i = 0; i < 7; i++) {
+            result = crossover(result);
+            result = selection(xu, yu, result);
         }
 
 
         }
 
-        public static int selection(ArrayList<Double> au,int min){
-            ArrayList<Double> selection = new ArrayList<>();
-
-            for (int i = 0; i < 100; i++) {
-                selection.add(au.get(min - i));
-                selection.add(au.get(min + i));
-            }
-
-            ArrayList<Double> selection2 = new ArrayList<>();
+        //교차 두 a값의 3/4지점과 1/4지점을 교차연산했다
+        public static ArrayList<Double> crossover(ArrayList<Double> au){
+        ArrayList<Double> crossover = new ArrayList<>();
             for (int i = 0; i < 50; i++) {
-                double sel1 = selection.get((int) (Math.random() * 100));
-                double sel2 = selection.get((int) (Math.random() * 100));
-                selection2.add(sel1 * ((double) 1 / (double) 4) + sel2 * ((double) 3 / (double) 4));
-                selection2.add(sel1 * ((double) 3 / (double) 4) + sel2 * ((double) 1 / (double) 4));
+                double sel1 = au.get((int) (Math.random() * 100));
+                double sel2 = au.get((int) (Math.random() * 100));
+                crossover.add(sel1 * ((double) 1 / (double) 4) + sel2 * ((double) 3 / (double) 4));
+                crossover.add(sel1 * ((double) 3 / (double) 4) + sel2 * ((double) 1 / (double) 4));
         }
-            return hubo(selection2);
+
+            return crossover;
     }
 
-public static int hubo(ArrayList<Double> au){
-    double a = 1.5;
-    double b = 100;
 
-    ArrayList<Double> xu = new ArrayList<>();
-    for (int i = 0; i < 100; i++) {
-        xu.add(i,Math.random()*(35-20+1)+20);
-    }
-
-    Random ran = new Random();
-    ArrayList<Double> yu = new ArrayList<>();
-    for (int i = 0; i < 100; i++) {
-        yu.add(i, a*xu.get(i)+b+ran.nextGaussian()*10);
-    }
-
-    ArrayList<Double>  mse = new ArrayList<>();
+//선택 연산
+public static ArrayList<Double> selection(ArrayList<Double> xu, ArrayList<Double> yu, ArrayList<Double> au){
+        ArrayList<Double>  mse = new ArrayList<>();
     double flag=50000;
     int min = 0;
     for (int i = 0; i < au.size(); i++) {
@@ -68,8 +77,22 @@ public static int hubo(ArrayList<Double> au){
             min = i;
         }
     }
-    System.out.println(au.get(min));
-        return min;
+
+
+    ArrayList<Double> selection = new ArrayList<>();
+    selection.add(0,au.get(min));
+    for (int i = 1; i < 50; i++) {
+        selection.add(au.get(min - i));
+        selection.add(au.get(min + i));
+    }
+    selection.add(au.get(min+50));
+
+        return selection;
+    }
+
+    public static void mutation(){
+        Random r = new Random();
+        double p = (double)1/100;
     }
     }
 
